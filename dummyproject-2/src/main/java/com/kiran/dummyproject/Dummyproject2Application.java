689f4b9ea369ce9425bb62be375aea28b7735975
Dandukirankumar <67386494+Dandukirankumar@users.noreply.github.com>
2022-07-03 16:53:16 +0530

    commiting

---
+++ dummyproject-2/src/main/java/com/kiran/dummyproject/Dummyproject2Application.java
@@ -0,0 +1,13 @@
+package com.kiran.dummyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummyproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummyproject2Application.class, args);
+	}
+	
+}
