6e42ee7cb2e721ded17fc20510c26cd5e72c8958
Dandukirankumar <67386494+Dandukirankumar@users.noreply.github.com>
2022-07-03 16:59:40 +0530

    new commit

---
+++ dummyproject-1/src/main/java/com/kiran/dummyproject/Dummyproject1Application.java
@@ -0,0 +1,13 @@
+package com.kiran.dummyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummyproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummyproject1Application.class, args);
+	}
+
+}
---
+++ dummyproject-1/src/test/java/com/kiran/dummyproject/Dummyproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.dummyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummyproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
