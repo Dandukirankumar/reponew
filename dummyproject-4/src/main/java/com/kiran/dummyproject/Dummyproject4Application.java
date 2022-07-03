273bdb78ab7a2cde51b6e437fa9f7e485f701f29
Dandukirankumar <67386494+Dandukirankumar@users.noreply.github.com>
2022-07-03 17:21:43 +0530

    testgithub

---
+++ dummyproject-4/src/main/java/com/kiran/dummyproject/Dummyproject4Application.java
@@ -0,0 +1,13 @@
+package com.kiran.dummyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummyproject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummyproject4Application.class, args);
+	}
+
+}
